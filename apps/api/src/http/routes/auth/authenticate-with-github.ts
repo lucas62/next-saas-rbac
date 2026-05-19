b1a821e12db2465c629aa76e8bd37369e2f3dbff
Lucas62 <lucas.pires100.la@gmail.com>
2026-05-19 15:07:52 -0300

    feat: add GitHub authentication route to API
    - Implemented a new route for authenticating users via GitHub OAuth.
    - Registered the authenticateWithGithub route in the Fastify server setup.
    - Integrated Zod for request validation and error handling for missing email.
    - Utilized Prisma for user and account management during the authentication process.

---
+++ apps/api/src/http/routes/auth/authenticate-with-github.ts
@@ -0,0 +1,139 @@
+import { FastifyInstance } from 'fastify'
+import { ZodTypeProvider } from 'fastify-type-provider-zod'
+import { z } from 'zod'
+
+import { prisma } from '@/lib/prisma'
+
+import { BadRequestError } from '../_errors/bad-request-error'
+
+export function authenticateWithGithub(app: FastifyInstance) {
+  app.withTypeProvider<ZodTypeProvider>().post(
+    '/sessions/github',
+    {
+      schema: {
+        tags: ['auth'],
+        summary: 'Authenticate with Github',
+        body: z.object({
+          code: z.string(),
+        }),
+        response: {
+          201: z.object({
+            token: z.string(),
+          }),
+          400: z.object({
+            message: z.string(),
+          }),
+        },
+      },
+    },
+    async (request, reply) => {
+      const { code } = request.body
+
+      const githubOAuthURL = new URL(
+        'https://github.com/login/oauth/access_token',
+      )
+      githubOAuthURL.searchParams.set(
+        'client_id',
+        process.env.GITHUB_CLIENT_ID as string,
+      )
+      githubOAuthURL.searchParams.set(
+        'client_secret',
+        process.env.GITHUB_CLIENT_SECRET as string,
+      )
+      githubOAuthURL.searchParams.set(
+        'redirect_uri',
+        process.env.GITHUB_REDIRECT_URI as string,
+      )
+      githubOAuthURL.searchParams.set('code', code)
+
+      const githubAccessTokenResponse = await fetch(githubOAuthURL, {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+        },
+      })
+
+      const githubAccessTokenData = await githubAccessTokenResponse.json()
+
+      const { access_token: githubAccessToken } = z
+        .object({
+          access_token: z.string(),
+          token_type: z.literal('bearer'),
+          scope: z.string(),
+        })
+        .parse(githubAccessTokenData)
+
+      const githubUserResponse = await fetch('https://api.github.com/user', {
+        headers: {
+          Authorization: `Bearer ${githubAccessToken}`,
+        },
+      })
+
+      const githubUserData = await githubUserResponse.json()
+
+      const {
+        id: githubUserId,
+        name,
+        email,
+        avatar_url: avatarUrl,
+      } = z
+        .object({
+          id: z.number().int().positive().transform(String),
+          name: z.string().nullable(),
+          email: z.email().nullable(),
+          avatar_url: z.url(),
+        })
+        .parse(githubUserData)
+
+      if (email === null) {
+        throw new BadRequestError(
+          'Your Github account must have an email to authenticate',
+        )
+      }
+
+      let user = await prisma.user.findUnique({
+        where: {
+          email,
+        },
+      })
+      if (!user) {
+        user = await prisma.user.create({
+          data: {
+            email,
+            name,
+            avatarUrl,
+          },
+        })
+      }
+
+      let account = await prisma.account.findUnique({
+        where: {
+          provider_userId: {
+            provider: 'GITHUB',
+            userId: user.id,
+          },
+        },
+      })
+      if (!account) {
+        account = await prisma.account.create({
+          data: {
+            userId: user.id,
+            provider: 'GITHUB',
+            providerAccountId: githubUserId,
+          },
+        })
+      }
+
+      const token = await reply.jwtSign(
+        {
+          sub: user.id,
+        },
+        {
+          expiresIn: '7d',
+        },
+      )
+
+      return reply.status(201).send({ token })
+    },
+  )
+}
