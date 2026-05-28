df8850a3ecc8d819291b111b201b403bd7f86417
Lucas62 <lucas.pires100.la@gmail.com>
2026-05-28 15:40:29 -0300

    feat: implement authentication API client with ky, update dependencies, and fix button component prop logic

--- apps/web/src/app/auth/sign-in/actions.tsx
+++ apps/web/src/app/auth/sign-in/actions.tsx
@@ -1,7 +1,18 @@
 'use server'
 
+import ky from 'ky'
+
+const api = ky.create({
+  prefix: process.env.NEXT_PUBLIC_API_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+})
+
 export async function signInWithEmailAndPassword(data: FormData) {
-  console.log(Object.fromEntries(data))
+  await api.post('auth/login', {
+    json: Object.fromEntries(data),
+  })
 }
 
 export async function signInWithGithub() {}
